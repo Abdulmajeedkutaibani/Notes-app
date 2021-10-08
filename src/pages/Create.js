ef3b25af9df2426fe32a83aff1d5dd4f18c2ba58
Abdulmajeed Kutaibani <Abodymeo@yahoo.com>
2021-10-08 22:01:07 +0300

    install material UI icons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Button, Container, Typography } from '@mui/material';
 
 export default function Create() {
   return (
-    <div>
-      Create page
-    </div>
-  )
+    <Container>
+      <Typography variant='h6' component='h2' gutterBottom>
+        create a new note
+      </Typography>
+      <Button
+        onClick={() => {
+          console.log('you clicked me');
+        }}
+        type='submit'
+        color='secondary'
+        variant='contained'
+      >
+        Submit
+      </Button>
+    </Container>
+  );
 }
