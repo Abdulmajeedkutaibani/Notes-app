95d537f466b79f48967fc08edb6fa7c5ea0a5030
Abdulmajeed Kutaibani <Abodymeo@yahoo.com>
2021-10-10 21:26:10 +0300

    add List to drawer

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import { Typography, Drawer } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import { display } from '@mui/system';
+import {
+  Typography,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
+import { AddCircleOutlined, SubjectOutlined } from '@mui/icons-material';
+import { useHistory, useLocation } from 'react-router';
 
 const drawerWidth = 240;
 
@@ -11,8 +18,23 @@ const classes = {
     width: '100%',
   },
 };
+const menuItems = [
+  {
+    Text: 'My Notes',
+    Icon: <SubjectOutlined color='secondary' />,
+    path: '/',
+  },
+  {
+    Text: 'Create Note',
+    Icon: <AddCircleOutlined color='secondary' />,
+    path: '/create',
+  },
+];
 
 const Layout = ({ children }) => {
+  const history = useHistory();
+  const location = useLocation();
+
   return (
     <div style={{ display: 'flex' }}>
       {/* app bar */}
@@ -30,6 +52,27 @@ const Layout = ({ children }) => {
         <div>
           <Typography variant='h5'>AK Notes</Typography>
         </div>
+
+        {/* list / links */}
+        <List>
+          {menuItems.map((item) => (
+            <ListItem
+              key={item.Text}
+              button
+              onClick={() => {
+                history.push(item.path);
+              }}
+              sx={
+                location.pathname == item.path
+                  ? { background: '#f4f4f4' }
+                  : null
+              }
+            >
+              <ListItemIcon>{item.Icon}</ListItemIcon>
+              <ListItemText primary={item.Text} />
+            </ListItem>
+          ))}
+        </List>
       </Drawer>
       <div style={classes.page}>{children}</div>
     </div>
