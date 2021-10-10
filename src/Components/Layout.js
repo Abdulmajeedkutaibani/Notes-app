3f680d0e601eb7627a0a929484f81300c773d016
Abdulmajeed Kutaibani <Abodymeo@yahoo.com>
2021-10-10 20:10:24 +0300

    added permanent drawer

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,16 +1,37 @@
 import React from 'react';
+import { Typography, Drawer } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { display } from '@mui/system';
+
+const drawerWidth = 240;
+
+const classes = {
+  page: {
+    background: '#f9f9f9',
+    width: '100%',
+  },
+};
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <div
+    <div style={{ display: 'flex' }}>
+      {/* app bar */}
+      {/* side drawer */}
+      <Drawer
         sx={{
-          background: '#f9f9f9',
-          width: '100%',
+          width: drawerWidth,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+          },
         }}
+        variant='permanent'
+        anchor='left'
       >
-        {children}
-      </div>
+        <div>
+          <Typography variant='h5'>AK Notes</Typography>
+        </div>
+      </Drawer>
+      <div style={classes.page}>{children}</div>
     </div>
   );
 };
